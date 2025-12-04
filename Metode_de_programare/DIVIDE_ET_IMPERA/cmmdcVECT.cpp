474dee52d0e674cbfb86d073394a8d117aef0f1d
pepinho2049 <mariussjohn@gmail.com>
2025-12-04 11:31:51 +0200

    ok

--- Metode_de_programare/DIVIDE_ET_IMPERA/cmmdcVECT.cpp
+++ Metode_de_programare/DIVIDE_ET_IMPERA/cmmdcVECT.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int n,v[50];
 
-int cmmdc(int a, int b){
-    if(b == 0) return a;
-    else return cmmdc(b, a%b);
+int cmmdc (int a, int b){
+    if(b == 0)
+        return a;
+    return cmmdc(b, a % b);
 }
-// varianta iterativă
-/*
-int cmmdcvect(int v[], int n){
-    int temp = v[0];
-    for(int i = 1; i <n; i++){
-        temp = cmmdc(temp, v[i]);
-    }
-    return temp;
-}
-*/
-
-//varianta Divide et Impera
 int divide(int v[], int st, int dr){
-    if(dr-st <=1) return cmmdc(v[st], v[dr]);
-    int mijl = (st+dr)/2;
-    return cmmdc(divide(v, st, mijl), divide(v, mijl+1, dr));   
+    if(st == dr) return v[st];
+    int middle = (st+dr)/2;
+    int cmmdcstanga = divide(v, st, middle);
+    int cmmdcdreapta = divide(v, middle+1, dr);
+    return cmmdc(cmmdcstanga, cmmdcdreapta);
 }
-
-
 int main(){
     cin >> n;
-    for(int i = 0; i < n; i++){
+    int v[n+1];
+    for(int i = 0; i < n; i++)
         cin >> v[i];
-    }
-    cout<< divide(v, 0, n-1);
+    cout << divide(v, 0, n - 1);
 }
